b4be9ba8f6a0a663b60829a2c8150527a7773955
pranit sarode <pranitsarode2003@gmail.com>
2024-04-06 19:29:08 +0530

    5 msg

--- img/button.js
+++ img/button.js
@@ -1,4 +1,5 @@
 document.getElementById("backToHomeButton").addEventListener("click", function() {
     // Replace "home.html" with the path to your home page
     window.location.href = "index.html";
+    
 });
